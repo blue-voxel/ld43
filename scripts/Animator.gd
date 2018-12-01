50676305a89e37671f8b6fc8dc8deb5535924e16
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2018-12-01 21:30:59 +0000

    cleaned up inMap and added get_look()

--- scripts/Animator.gd
+++ scripts/Animator.gd
@@ -1,8 +1,6 @@
 extends Sprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
--- scripts/inMap.gd
+++ scripts/inMap.gd
@@ -1,22 +1,43 @@
 #script must be added as a singleton in Project-Settings to function properly
 extends Node 
 
+var joypad = false
+
 var move = Vector2()
+var look = 0.0
 
 func _ready():
     
     pass
 
 func _process(delta):
-	move = Vector2()
-	move.x += float(Input.is_action_pressed('ui_right')) + Input.get_joy_axis(0,0)
-	move.x -= float(Input.is_action_pressed('ui_left'))
-	move.y += float(Input.is_action_pressed('ui_down')) + Input.get_joy_axis(0,1)
-	move.y -= float(Input.is_action_pressed('ui_up'))
-	
-	if 1 < move.length_squared():
-		move = move.normalized()
+	joypad = 0 < len(get_connected_joypads())
+	get_move()
 	pass
 
 func get_move():
+	move = Vector2()
+	move.x += float(Input.is_action_pressed('ui_right'))
+	move.x -= float(Input.is_action_pressed('ui_left'))
+	move.y += float(Input.is_action_pressed('ui_down'))
+	move.y -= float(Input.is_action_pressed('ui_up'))
+	
+	if joypad:
+		move.x += Input.get_joy_axis(0,0)
+		move.y += Input.get_joy_axis(0,1)
+	
+	if 1 < move.length_squared():
+		move = move.normalized()
 	return move
+
+func get_look(from):
+	if joypad:
+		look = Vector2( Input.get_joy_axis(0,2),Input.get_joy_axis(0,3)).angle()
+	else:
+		mouse = get_viewport().get_mouse_position()
+		if from:
+			look = mouse.angle_to(from)
+		else:
+			look = mouse.angle()
+	look = rad2deg(look)
+	return look
