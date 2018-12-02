a03767d29fff2c53935a43192e237aec052c82ea
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2018-12-02 23:37:21 +0000

    half-implemented projectile collisions

--- scripts/Area2D/projectile.gd
+++ scripts/Area2D/projectile.gd
@@ -11,5 +11,22 @@ func _ready():
 	transpose = Vector2(cos(direction), sin(direction)) * speed
 	pass
 
-func _process(delta):
+func _physics_process(delta):
+	if 0 < len(get_overlapping_bodies()):
+		on_hit(get_overlapping_bodies()[0])
 	position += transpose * delta
+
+
+func on_hit(body):
+	print (body)
+	die()
+
+func die():
+	queue_free()
+
+func damage(amount):
+	die()
+
+
+func _on_hit(body):
+	pass # replace with function body
