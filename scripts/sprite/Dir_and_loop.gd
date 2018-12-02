9e5deb051c45999ca8448881eed9266fcfb3e1a2
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2018-12-02 18:15:13 +0000

    added and animated magic missile

---
+++ scripts/sprite/Dir_and_loop.gd
@@ -0,0 +1,28 @@
+extends "res://scripts/sprite/Dir_animator.gd"
+
+export (float) var framerate = 1
+export (bool) var reflect = false
+
+var time = 0
+var frame_duration
+var reflecting
+
+func _ready():
+    ._ready()
+    frame_duration = 1 / framerate
+    ._process(0)
+
+func _process(delta):
+    time += delta
+    
+    if frame_duration < time:
+        if sliceY * (texY-1) <= region_rect.position.y:
+            if reflect:
+                reflecting = true
+            else:
+                region_rect.position.y = -sliceY
+        region_rect.position.y += sliceY * (-1 if reflecting else 1)
+        if reflecting and region_rect.position.y < sliceY:
+            reflecting = false
+        
+        time -= frame_duration
