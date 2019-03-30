ab0f4898d88fd918dd3b23ef4a049df33adeaec8
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2019-03-30 13:04:32 +0000

    fixed primary movement controller and made minor tweaks

--- scripts/KB2D/simpleMoveK2D.gd
+++ scripts/KB2D/simpleMoveK2D.gd
@@ -5,22 +5,20 @@ export (float) var damping = 0.5 #time (seconds) to reach top speed
 
 var cutoff = 0.1 #acceptable threshold to target speed
 var vel = Vector2()
-var col
 
 func _ready():
-	topSpeed /= 12 #idk why it just works
 	pass
 
 func _physics_process(delta):
-	var target = inMap.move * topSpeed if not col else Vector2()
+	var target = inMap.move * topSpeed
 	if 0 < damping:
 		var dVel = target - vel #difference between target velocity and current velocity
 		if cutoff < dVel.length_squared(): 
-			vel += dVel.normalized() * topSpeed * delta / damping #normalized() ensures that acceleration is constant (not proportional to distance to target)
+			vel += dVel.normalized() * topSpeed * delta / damping #normalized() ensures that acceleration is constant (not proportional to distance to target). Note that this line affects acceleration, not speed
 		else:
 			vel = target
 	else:
 		vel = target
 	
-	col = move_and_collide(vel)
+	move_and_slide(vel)
 	pass
